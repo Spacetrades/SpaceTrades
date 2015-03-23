9dec6cbb5656f88b8889c371771950f6a917dfdf
chackerian <nchackerian@gmail.com>
2015-03-23 01:35:19 -0400

    Removed unused bash scripts

--- exec1.sh
+++
@@ -1,7 +0,0 @@
-myprog=/bin/ls
-echo "This is the wrapper script, it will exec $myprog"
- 
-# do some vodoo here, probably change the arguments etc.
-# well, stuff a wrapper is there for
- 
-exec "$myprog" "$@"
