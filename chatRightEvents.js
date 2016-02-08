c8603660d30166ffdd9f28902792ecd34d016b16
chackerian <nchackerian@gmail.com>
2016-02-08 11:37:24 -0500

    chat

--- chatRightEvents.js
+++ chatRightEvents.js
@@ -8,17 +8,20 @@ if (Meteor.isClient) {
 				sender: Meteor.userId(),
 				receiver: id,
 				// Conversation key will be generated dynamically and used for all conversation messages
-				conversation: "rename",
+				conversation: Random.id(7),
         createdAt: new Date()
 			};
 
       // FORMAT time
       var time = moment(options.createdAt);
-      time = time.format("dddd MMM, DD");
+      time = time.format("h:mm, dddd MMM, DD");
       options.createdAt = time;
 
 
-			Meteor.call('sendMessage', options)
+			Meteor.call('sendMessage', options);
+
+// IF message is a divider - new message after other person talking or just new from the start
+      // if ();
 
 			// Clears message input
 			$(".chatText").val('');
