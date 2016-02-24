8331b8b8cda75df5161424278e66a3b28eb27b1f
chackerian <nchackerian@gmail.com>
2016-02-24 01:23:12 -0500

    activeCard

--- client/helpers/Modal/modalActiveCardHelpers.js
+++ client/helpers/Modal/modalActiveCardHelpers.js
@@ -1,9 +1,35 @@
 if (Meteor.isClient) {
 
   Template.ModalProfileActive.helpers({
-    scope: function(){
+    scope: function() {
       return Session.get('activeSelected');
-    }
+    },
+    nameOther: function() {
+      if (Meteor.userId() == this.creator_id) {
+        return this.offer_creator_name;
+      } else {
+        return this.username;
+      }
+    },
+    timeUntil: function() {
+
+    },
+    chatId: function() {
+      var currentConversation = Message.find({
+        receiver: Session.get('otherId')
+      }).fetch();
+      var convoState = !_.isEmpty(currentConversation);
+
+      if (convoState) {
+
+        var convo_id = currentConversation[0].conversation;
+        return convo_id;
+      } else {
+
+        var convo_id = Random.id(10);
+        return convo_id
+      }
+    },
   });
 
 }
