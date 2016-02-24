a4bad06652cc2fdea959ed3c6a88051e695cecbd
chackerian <nchackerian@gmail.com>
2016-02-24 01:41:41 -0500

    FIX

--- client/helpers/Modal/modalOfferRequestHelpers.js
+++ client/helpers/Modal/modalOfferRequestHelpers.js
@@ -3,23 +3,21 @@ if (Meteor.isClient) {
 	Template.ModalOfferRequest.helpers({
 		offerMapOptions: function() {
 			if (GoogleMaps.loaded()) {
-				var lat = Listing.find({
-					_id: id
-				}).fetch()[0].lat;
-				var lng = Listing.find({
-					_id: id
-				}).fetch()[0].lng;
-				var map = new google.maps.LatLng(lat, lng);
+
+				// var lat = Listing.find({
+				// 	_id: this.id
+				// }).fetch()[0].lat;
+				// var lng = Listing.find({
+				// 	_id: id
+				// }).fetch()[0].lng;
+
+				var map = new google.maps.LatLng(this.lat, this.lng);
 				return {
 					center: map,
 					zoom: 10
 				};
 			}
-		},
-    payment: function(payment){
-      var status = payment == Session.get("scope").payment;
-      return status
-    }
+		}
 	});
 
 }
