7d00293c7bf09e8cea915458430476086f734335
chackerian <nchackerian@gmail.com>
2016-03-03 00:49:10 -0500

    STRUCT

--- client/events/Modal/modalFeedbackInitialEvents.js
+++ client/events/Modal/modalFeedbackInitialEvents.js
@@ -2,8 +2,29 @@ if (Meteor.isClient) {
 
   Template.ModalFeedbackInitial.events({
 
+    'click .step1 .yes': function() {
+      $('.step-1').hide();
+      $('.step-3').show();
+    },
+    'click .step1 .no': function() {
+      $('.step-1').hide();
+      $('.step-2').show();
+    },
+    'click .success': function() {
+      $('.step-3').hide();
+      $('.step-3').show();
+    },
+    'click .nosuccess': function() {
+      $('.step-3').hide();
+      $('.step-6').show();
 
+    },
+    'click .notNext': function() {
+      $('.step-2').hide();
+      $('.step-5').show();
 
-  })
+    }
+
+  });
 
 }
