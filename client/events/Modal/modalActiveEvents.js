14941469f33c4592d560f5cc6786c4d332f0d96b
chackerian <nchackerian@gmail.com>
2016-03-13 18:39:07 -0400

    modal

--- client/events/Modal/modalActiveEvents.js
+++ client/events/Modal/modalActiveEvents.js
@@ -1,9 +1,12 @@
-if ( Meteor.isClient) {
+if (Meteor.isClient) {
 
-Template.ModalProfileActive.events({
-  'click .modActiveCreator': function () {
-   $("#ModalProfileActive").modal('hide');
-  }
-});
+  Template.ModalProfileActive.events({
+    'click .modActiveCreator': function() {
+      $("#ModalProfileActive").modal('hide');
+    },
+    'click .modProfileActiveBtn': function() {
+      $("#ModalProfileActive").modal('hide')
+    }
+  });
 
 }
