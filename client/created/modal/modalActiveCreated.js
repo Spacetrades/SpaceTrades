5cd3e3a09d63abeda2d1a0518a38d8092b71769e
chackerian <nchackerian@gmail.com>
2016-03-22 16:06:44 -0400

    changes

--- client/created/modal/modalActiveCreated.js
+++ client/created/modal/modalActiveCreated.js
@@ -2,31 +2,19 @@
 
      Template.ModalProfileActive.onCreated(function() {
 
-// Run Immediately
-      var original = moment.unix(Session.get('activeSelected').meetup_time);
-           var delayTime = original.diff(moment());
-           var delayTimeMinutes = delayTime / 1000 / 60;
-           var hours = Math.floor(delayTimeMinutes / 60);
-           var minutes = Math.floor(delayTimeMinutes % 60);
-           var timeString = hours + " hours and " + minutes + " minutes";
-           Session.set('timeString', timeString);
-// Run on
+       // Run Immediately
+       try {
+         var original = moment.unix(Session.get('activeSelected').meetup_time);
+         var delayTime = original.diff(moment());
+         var delayTimeMinutes = delayTime / 1000 / 60;
+         var hours = Math.floor(delayTimeMinutes / 60);
+         var minutes = Math.floor(delayTimeMinutes % 60);
+         var timeString = hours + " hours and " + minutes + " minutes";
+         Session.set('timeString', timeString);
+       } catch (error) {;
+       }
 
 
-           var original = moment.unix(Session.get('activeSelected').meetup_time);
-           var delayTime = original.diff(moment());
-           var delayTimeMinutes = delayTime / 1000 / 60;
+     })
 
-           var hours = Math.floor(delayTimeMinutes / 60);
-           var minutes = Math.floor(delayTimeMinutes % 60);
-
-           var timeString = hours + " hours and " + minutes + " minutes";
-
-           Session.set('timeString', timeString);
-
-
-       })
-
-     }
-
-
+   }
