63b2665965be184801409d1372d654f7a53fc5fb
chackerian <nchackerian@gmail.com>
2016-03-28 06:46:05 -0400

    contactpre

---
+++ client/events/Help/contactPreEvents.js
@@ -0,0 +1,22 @@
+if (Meteor.isClient) {
+
+    Template.contactPre.events({
+        'click .contactButton': function(e) {
+            e.preventDefault();
+
+            var subject = $("#contactname").val();
+            var text = $(".message").val();
+            var name = Meteor.user().profile.name;
+            sAlert.success('Message sent');
+
+
+            Meteor.call('sendEmail',
+                'nchackerian@gmail.com',
+                'contact@spacetrades.com',
+                subject,
+                text
+            );
+        }
+    });
+
+}
