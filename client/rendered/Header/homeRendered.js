a47d61ba5323bf4fcedb6e3b4effb5f27513b350
chackerian <nchackerian@gmail.com>
2016-03-28 12:16:25 -0400

    LOTS

--- client/rendered/Header/homeRendered.js
+++ client/rendered/Header/homeRendered.js
@@ -1,25 +1,35 @@
 if (Meteor.isClient) {
 
-    Template.home.onRendered(function() {
+  Template.home.onRendered(function() {
 
-        $('homeCardImg').error( function(){
-          this.attr('src', 'default-item-img.jpg');
-        });
-
-        keyPress.home("search");
-
-        var tags = ["Air Jordan", "Aeropostale", "Nike", "Air Force One", "nike"];
-        $("#search").autocomplete({
-            delay: 200,
-            source: tags
-        });
-
-        GoogleMaps.load({
-            v: '3',
-            key: 'AIzaSyAi0bRmwNIWv24KjjeiG0DlcU-jFLPJ9FQ',
-            libraries: 'geometry,places'
-        });
-
+    $('homeCardImg').error(function() {
+      this.attr('src', 'default-item-img.jpg');
     });
+
+    keyPress.home("search");
+
+    var keys = Listing.find({}, {
+      fields: {
+        listing_title: 1
+      }
+    }).fetch()
+
+    var tags = [];
+    _.map(keys, function(x) {
+      tags.push(x.listing_title);
+    })
+
+    $("#search").autocomplete({
+      delay: 200,
+      source: tags
+    });
+
+    GoogleMaps.load({
+      v: '3',
+      key: 'AIzaSyAi0bRmwNIWv24KjjeiG0DlcU-jFLPJ9FQ',
+      libraries: 'geometry,places'
+    });
+
+  });
 
 }
